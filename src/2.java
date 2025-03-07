db283f86654afe612527ce0ebe5eca891264bd8a
alexandrawilliam404 <alexandrawilliam404@gmail.com>
2025-03-07 12:18:55 +0000

    Add 2.java

---
+++ src/2.java
@@ -0,0 +1,9 @@
+package com.example;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int num = rand.nextInt(10);
+        System.out.println("The number is: " + num);
+    }
+}
