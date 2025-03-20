e692a85100c93a99c33f55b9cb095d6672774ad7
alexandrawilliam404 <alexandrawilliam404@gmail.com>
2025-03-20 01:09:44 +0000

    Add 15.java

---
+++ src/15.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class ClassroomWork {
+  public static void main(String[] args) {
+    Random rand = new Random();
+    int num = rand.nextInt(100);
+    System.out.println("Your random number is: " + num);
+  }
+}
