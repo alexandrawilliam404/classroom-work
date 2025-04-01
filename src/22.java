90955c416856bbea4808d7a75ced121f18a1de3c
alexandrawilliam404 <alexandrawilliam404@gmail.com>
2025-04-01 15:29:34 +0000

    Add 22.java

---
+++ src/22.java
@@ -0,0 +1,17 @@
+public class ClassroomWork {
+    public static void main(String[] args) {
+        // Example 1: Print "Hello, World!"
+        System.out.println("Hello, World!");
+
+        // Example 2: Add two numbers
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+        // Example 3: Print an empty string
+        String text = "";
+        System.out.println(text);
+    }
+}
