5b76fb21318a9c063e27a54001d8f109be69e2f8
alexandrawilliam404 <alexandrawilliam404@gmail.com>
2025-04-07 14:40:25 +0000

    Add 27.java

---
+++ src/27.java
@@ -0,0 +1,16 @@
+public class ClassroomWork {
+    public static void main(String[] args) {
+        // Example code for a simple class exercise
+        int age = 18;
+        String name = "Alice";
+        boolean isAdult = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Name: " + name);
+        if (isAdult) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are not an adult.");
+        }
+    }
+}
