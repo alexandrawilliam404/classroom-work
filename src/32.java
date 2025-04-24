fa2aec606c9915cb327bbb3e73d90d4b636abf0c
alexandrawilliam404 <alexandrawilliam404@gmail.com>
2025-04-24 17:55:49 +0000

    Add 32.java

---
+++ src/32.java
@@ -0,0 +1,6 @@
+public class ClassroomWork {
+    public static void main(String[] args) {
+        // Code to be executed in the classroom/workplace
+        System.out.println("Hello, this is a Java program.");
+    }
+}
